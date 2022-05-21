5c0ee343dc6310d3886be557963188307255df50
Sam Wei Hong <weihongsam2002@gmail.com>
2022-05-21 19:14:45 +0800

    :sparkles: Add Utils class with formatList()

---
+++ src/main/java/Utils.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Utils {
+    public static String formatList(List<?> list) {
+        if (list.isEmpty()) return "";
+        if (list.size() == 1) return list.get(0).toString();
+
+        StringBuilder res = new StringBuilder();
+        res.append(list.get(0));
+        for (int i = 1; i < list.size()-1; i++)
+            res.append(", ").append(list.get(i));
+        res.append(" and ").append(list.get(list.size() - 1));
+        return res.toString();
+    }
+}
