1859bd2cf1c7a9c716474c5c077594bc4431077b
millivan <limzyu02@gmail.com>
2022-05-29 11:38:47 +0800

    :sparkles: Updated TitanEvaluationAndKillingPriority to accept input and randomly generate titans

--- src/main/java/TitanEvaluationAndKillingPriority.java
+++ src/main/java/TitanEvaluationAndKillingPriority.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TitanEvaluationAndKillingPriority {
 
@@ -15,6 +16,34 @@ public class TitanEvaluationAndKillingPriority {
         }
 
         return dangerRisks;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number of Titans: ");
+        int noOfTitans = scanner.nextInt();
+
+        System.out.println();
+        List<Integer> dangerRiskList = new ArrayList<>();
+        System.out.printf("Generating %d Titans ...\n", noOfTitans);
+        for(int i = 1; i <= noOfTitans; i++){
+            Titan titan = new Titan();
+            System.out.printf("Titan %d: %s\n", i, titan);
+            dangerRiskList.add(titan.getRisk(titan.toString()));
+        }
+
+        System.out.println();
+        System.out.println("Their respective danger risks: ");
+        for(int i = 1; i <= noOfTitans; i++){
+            System.out.printf("Titan %d Risk: %d\n", i, dangerRiskList.get(i-1));
+        }
+
+        System.out.println();
+        System.out.print("Sequence to be killed: ");
+        System.out.print("Total distance moved: ");
 
     }
 
