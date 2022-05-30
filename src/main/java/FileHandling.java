14872958ef3f5c1e541e3e41e300ef7b293058be
damiafatihah <damiafatihah93@gmail.com>
2022-05-30 08:29:42 +0800

    Change to LinkedList

--- src/main/java/FileHandling.java
+++ src/main/java/FileHandling.java
@@ -1,61 +1,31 @@
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-
 import java.io.*;
 
 public class FileHandling {
 
-//    public void writeInFile(Character character,String path) {
-//
-//        File file = new File(path);
-//
-//        if (!file.exists()) {
-//            try {
-//                file.createNewFile();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        try ( final FileWriter fileWriter = new FileWriter(file) ) {
-//
-//            new Gson().toJson(character, fileWriter);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    static LinkedList<GameCharacter> characters = new LinkedList<>();
 
-    public void writeInFile(Character[] character,String path) {
+    public static LinkedList<GameCharacter> getCharacterList() {
+        return readFromFile("myjson.json");
+    }
 
+    public static void writeInFile(LinkedList<GameCharacter> characters, String path) {
         File file = new File(path);
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
         try ( final FileWriter fileWriter = new FileWriter(file) ) {
-
-            new Gson().toJson(character, fileWriter);
+            new Gson().toJson(characters, fileWriter);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Character[] readFromFile(String path) {
+    public static LinkedList<GameCharacter> readFromFile(String path) {
         final File file = new File(path);
-
         try ( final FileReader fileReader = new FileReader(file) ) {
-            final Character[] character = new Gson().fromJson(fileReader, Character[].class);
-            return character;
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            final GameCharacter[] character = new Gson().fromJson(fileReader, GameCharacter[].class);
+            for(GameCharacter cha : character) characters.add(cha);
+            return characters;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
