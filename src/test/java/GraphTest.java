40166d6ab88490102df27c86ce8ecb27f17e7cdc
millivan <limzyu02@gmail.com>
2022-06-05 17:38:56 +0800

    Created test class for findPaths method

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -12,59 +12,52 @@ class GraphTest {
     @Test
     void testFindPaths(){
 
-        Queue<List<List<Integer>>> data = getTestData();
+        List<List<Integer>> map = Graph.mapOfParadis;
+        
+        Queue<Integer> data = getTestData();
         assertNotNull(data);
 
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 1, 6),
                 Arrays.asList(0, 5, 6)
-        ), Graph.findPaths(data.poll(), 0, 6));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 1, 6, 15),
+                Arrays.asList(0, 5, 6, 15),
                 Arrays.asList(0, 7, 9, 15)
-        ), Graph.findPaths(data.poll(), 0, 15));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 1)
-        ), Graph.findPaths(data.poll(), 0, 1));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 5)
-        ), Graph.findPaths(data.poll(), 0, 5));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 7, 9)
-        ), Graph.findPaths(data.poll(), 0, 9));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
+                Arrays.asList(0, 1, 2, 13, 14),
                 Arrays.asList(0, 1, 4, 10, 14),
                 Arrays.asList(0, 1, 6, 15, 14),
-                Arrays.asList(0, 1, 2, 13, 14)
-        ), Graph.findPaths(data.poll(), 0, 14));
+                Arrays.asList(0, 5, 6, 15, 14),
+                Arrays.asList(0, 7, 9, 15, 14)
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
                 Arrays.asList(0, 5, 12)
-        ), Graph.findPaths(Graph.mapOfParadis, 0, 12));
+        ), Graph.findPaths(map, 0, data.poll()));
         assertEquals(Arrays.asList(
-                Arrays.asList(0, 1, 6, 8)
-        ), Graph.findPaths(data.poll(), 0, 8));
+                Arrays.asList(0, 1, 6, 8),
+                Arrays.asList(0, 5, 6, 8)
+        ), Graph.findPaths(map, 0, data.poll()));
 
 
     }
 
-    private Queue<List<List<Integer>>> getTestData() {
-        Queue<List<List<Integer>>> data = new ArrayDeque<>();
+    private Queue<Integer> getTestData() {
+        Queue<Integer> data = new ArrayDeque<>();
         try (Scanner input = new Scanner(new FileInputStream("src/test/resources/test-data/bestPathToKillTitanTest.txt"))) {
-            while (input.hasNextLine()) {
-                List<List<Integer>> unitTest = new ArrayList<>();
-                int dest = input.nextInt();
-                int noOfPaths = input.nextInt();
-                input.nextLine();
-                for (int i = 0; i < noOfPaths; i++) {
-                    String path = input.nextLine();
-                    String[] pathIndices = path.split(" ");
-                    ArrayList<Integer> intPathIndices = new ArrayList<>();
-                    for(int j = 0; j < pathIndices.length; j++){
-                        int a = Integer.parseInt(pathIndices[j]);
-                        intPathIndices.add(a);
-                    }
-                    unitTest.add(intPathIndices);
-                }
+            while (input.hasNextInt()) {
+                Integer unitTest = input.nextInt();
                 data.offer(unitTest);
             }
         } catch (FileNotFoundException e) {
