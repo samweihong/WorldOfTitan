d33c7c9fe5398c8378f7b9d9d855d336d7b2b620
millivan <limzyu02@gmail.com>
2022-06-05 09:38:04 +0800

    Modified Map Of Paradis to a variable

--- src/main/java/logic/Graph.java
+++ src/main/java/logic/Graph.java
@@ -4,6 +4,25 @@ import java.util.*;
 
 public class Graph{
 
+    public static List<List<Integer>> mapOfParadis = new ArrayList(Arrays.asList(
+            Arrays.asList(1, 5, 7),
+            Arrays.asList(0, 2, 4, 6),
+            Arrays.asList(1, 3, 11, 13),
+            Arrays.asList(2, 10),
+            Arrays.asList(1, 6, 10),
+            Arrays.asList(0, 6, 7, 12),
+            Arrays.asList(1, 4, 5, 8, 15),
+            Arrays.asList(0, 5, 9),
+            Arrays.asList(6, 10),
+            Arrays.asList(7, 12, 15),
+            Arrays.asList(3, 4, 8, 14),
+            Arrays.asList(2, 13),
+            Arrays.asList(5, 9),
+            Arrays.asList(2, 11, 14),
+            Arrays.asList(10, 13, 15),
+            Arrays.asList(6, 9, 14)
+    ));
+
     public static String printPath(List<Integer> path){
         String direction = "";
         for(Integer v : path) direction += (v + "->");
@@ -49,29 +68,8 @@ public class Graph{
 
     public static void main(String[] args) {
 
-        // Creating map of Paradis
-        List<List<Integer>> map = new ArrayList<>();
-        for(int i = 0; i < 16; i++) map.add(new ArrayList<>());
-
-        map.get(0).addAll(Arrays.asList(1, 5, 7));
-        map.get(1).addAll(Arrays.asList(0, 2, 4, 6));
-        map.get(2).addAll(Arrays.asList(1, 3, 11, 13));
-        map.get(3).addAll(Arrays.asList(2, 10));
-        map.get(4).addAll(Arrays.asList(1, 6, 10));
-        map.get(5).addAll(Arrays.asList(0, 6, 7, 12));
-        map.get(6).addAll(Arrays.asList(1, 4, 5, 8, 15));
-        map.get(7).addAll(Arrays.asList(0, 5, 9));
-        map.get(8).addAll(Arrays.asList(6, 10));
-        map.get(9).addAll(Arrays.asList(7, 12, 15));
-        map.get(10).addAll(Arrays.asList(3, 4, 8, 14));
-        map.get(11).addAll(Arrays.asList(2, 13));
-        map.get(12).addAll(Arrays.asList(5, 9));
-        map.get(13).addAll(Arrays.asList(2, 11, 14));
-        map.get(14).addAll(Arrays.asList(10, 13, 15));
-        map.get(15).addAll(Arrays.asList(6, 9, 14));
-
         // finding shortest path
-        findpaths(map, 0, 6);
+        findpaths(Graph.mapOfParadis, 0, 6);
 
     }
 }
