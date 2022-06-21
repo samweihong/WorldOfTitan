dea3f37c81bbbd1922023c42195b704893be1e27
Sam Wei Hong <weihongsam2002@gmail.com>
2022-06-21 09:48:29 +0800

    :rocket: Add UI Part 1 & 2

--- src/main/java/logic/GameCharacterList.java
+++ src/main/java/logic/GameCharacterList.java
@@ -3,6 +3,7 @@ package logic;
 import collections.LinkedList;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data_objects.GameCharacter;
+import data_objects.MapOfParadis;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameCharacterList {
@@ -141,17 +143,113 @@ public class GameCharacterList {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter name: ");
         String name = scanner.nextLine();
-        System.out.print("Enter characteristics: ");
+        System.out.println("\nHeight Weight Strength Agility Intelligence Coordination Leadership");
+        System.out.print("Enter characteristics as in above format: ");
         int[] characteristics = Arrays.stream(scanner.nextLine().split(" "))
                                     .mapToInt(Integer::parseInt)
                                     .toArray();
         addGameCharacter(name, characteristics);
         System.out.println();
         System.out.println(gameCharacterList.get(0));
+        System.out.println("This character is added!\n");
+    }
+
+    public static void startGameCharacterIO() {
+        String menu = """
+                0. Quit
+                1. Add new character
+                2. View all character names
+                3. Get character information
+                4. Save characters
+                5. Load characters
+                6. Clear all characters
+                """;
+        Scanner scanner = new Scanner(System.in);
+
+        for (int choice = -1; choice != 0; ) {
+            System.out.println(menu);
+
+            while (true) {
+                System.out.print("Enter your choice: ");
+                try {
+                    choice = scanner.nextInt();
+                    if (choice < 0 || choice > 6) throw new IllegalArgumentException();
+                    break;
+                } catch (InputMismatchException | IllegalArgumentException e) {
+                    System.out.println("Invalid input! Please enter again.\n");
+                    scanner = new Scanner(System.in);
+                }
+            }
+            System.out.println();
+
+            scanner = new Scanner(System.in);
+            switch (choice) {
+                case 1 -> gameCharacterIO();
+                case 2 -> getGameCharacterNames();
+                case 3 -> {
+                    System.out.print("Enter the character's name : ");
+                    String name = scanner.nextLine();
+                    GameCharacter gameCharacter = getGameCharacter(name);
+                    System.out.println();
+                    System.out.println(gameCharacter == null ? "This character doesn't exist!" : gameCharacter);
+                }
+                case 4 -> {
+                    System.out.print("Enter the filename to save : ");
+                    String filename = scanner.nextLine();
+                    String path = System.getProperty("user.home") + "/" + filename + ".json";
+                    writeToFile(path);
+                    System.out.println("The characters are saved in " + path + "!");
+                }
+                case 5 -> {
+                    System.out.print("Enter the filename to load : ");
+                    String filename = scanner.nextLine();
+                    String path = System.getProperty("user.home") + "/" + filename + ".json";
+                    readFromFile(path);
+                    System.out.println("The characters are loaded from " + path + "!");
+                }
+                case 6 -> {
+                    clear();
+                    System.out.println("All characters have been cleared.");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void getGameCharacterNames() {
+        if (gameCharacterList.isEmpty()) {
+            System.out.println("The character list is empty!");
+            return;
+        }
+        int i = 1;
+        for (GameCharacter character : gameCharacterList)
+            System.out.printf("%2d. %s\n", i++, character.name());
+    }
+
+    public static void startSortAndSearch() {
+        System.out.println("""
+                1. Soldiers Arrangement
+                2. Soldiers Grouping
+                """);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        while (true) {
+            System.out.print("Enter your choice: ");
+            try {
+                choice = scanner.nextInt();
+                if (choice < 1 || choice > 2) throw new IllegalArgumentException();
+                break;
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.println("Invalid input! Please enter again.\n");
+                scanner = new Scanner(System.in);
+            }
+        }
+        System.out.println();
+        if (choice == 1) sort();
+        if (choice == 2) search();
     }
 
     private static void sort() {
-        readFromFile();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Sorting attribute: ");
         String attribute = scanner.nextLine();
@@ -161,14 +259,13 @@ public class GameCharacterList {
     }
 
     private static void search() {
-        readFromFile();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Finding ability : ");
         String attribute = scanner.nextLine();
-        System.out.println();
-        System.out.print("value: ");
+        System.out.print("With value of : ");
         int value = scanner.nextInt();
         System.out.println();
         System.out.println("Soldier : " + getSearchList(attribute, value));
+        System.out.println();
     }
 }
